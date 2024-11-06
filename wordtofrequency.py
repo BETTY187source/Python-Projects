167c2b525e238684fb30efe4b5a356b5d4aaa047
BETTY BENIMANA <bbenimanabetty@gmail.com>
2024-11-06 11:45:25 -0800

    Add files via upload

---
+++ wordtofrequency.py
@@ -0,0 +1,29 @@
+'''
+write a function that takes a sentence as input and returns a dictionary with words as keys and their frequency as values.
+'''
+def word_to_frequency():
+    sentence = input("enter a sentence: ").split()
+    wordfrequency = {}
+    for w in sentence:
+        if w in wordfrequency:
+            wordfrequency[w] += 1
+        else:
+            wordfrequency[w] = 1 # frequency for the word appearing for the first time
+    return wordfrequency
+
+# Calling for the function
+print(word_to_frequency())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
